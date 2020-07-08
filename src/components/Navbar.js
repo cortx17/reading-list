0644d4f784578ad83e1d6e206e103acba79014ba
cortx17 <pulsar.zero@gmail.com>
2020-07-08 18:47:34 +0200

    basic app with components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav>
-                <h1>Context App</h1>
+                <h1>Context App.</h1>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
